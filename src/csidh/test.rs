8c6f0c1869827f9686305650c3d873b007926d67
jetSett <joel.felderhoff@ens-lyon.fr>
2019-02-26 17:19:14 +0100

    Test for typed CSIDH

--- src/csidh/test.rs
+++ src/csidh/test.rs
@@ -1,15 +1,25 @@
 use super::*;
 
-fn get_global_instance() -> CSIDHInstance{
-    CSIDHInstance{ 
-            n_primes: 13,
-            p: Integer::from_str_radix("37118532150319619", 10).unwrap(),
-            l:vec![Integer::from(3),Integer::from(5),Integer::from(7),
+use elliptic_curve_algorithms::finite_fields::*;
+
+elliptic_curve_algorithms::declare_finite_field!(K, Integer, 
+            Integer::from_str_radix("37118532150319619", 10).unwrap(), 
+            _m);
+
+
+type PublicKey = K;
+type SecretKey = Vec<i32>;
+
+
+fn get_global_instance() -> CSIDHInstance<K>{
+    CSIDHInstance::new(13,
+            Integer::from_str_radix("37118532150319619", 10).unwrap(),
+            vec![Integer::from(3),Integer::from(5),Integer::from(7),
                     Integer::from(11),Integer::from(13),Integer::from(17),
                     Integer::from(19),Integer::from(23),Integer::from(29),
                     Integer::from(31),Integer::from(37),Integer::from(41),
                     Integer::from(61)]
-        }
+    )
 }
 
 fn sample_montgomery() -> EllipticCurve<K>{
@@ -18,7 +28,7 @@ fn sample_montgomery() -> EllipticCurve<K>{
 
     let mut a = K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(inst.p.clone())));
     let mut ell = EllipticCurve::new_montgomery(a.clone());
-    while ! (is_supersingular(&inst, &ell) && 
+    while ! (inst.is_supersingular(&ell) && 
             ell.discriminant() != K::from_int(0)) {
         a = K::new(Integer::sample_uniform(&Integer::from(0), &Integer::from(inst.p.clone())));
         ell = EllipticCurve::new_montgomery(a);
@@ -30,7 +40,7 @@ fn sample_montgomery() -> EllipticCurve<K>{
 fn everything_ok() {
     let inst = get_global_instance();
 
-    check_well_defined(&inst);
+    inst.check_well_defined();
 }
 
 #[test]
@@ -39,8 +49,8 @@ fn known_supersingular_ec(){
     let inst = get_global_instance();
 
     // Note P = 3 mod 4, 2 mod 3
-    assert!(is_supersingular(&inst, &EllipticCurve::new_reduced_weierstrass(K::from_int(1), K::from_int(0)))); // supersingular iff P = 3 mod 4
-    assert!(is_supersingular(&inst, &EllipticCurve::new_reduced_weierstrass(K::from_int(0), K::from_int(1)))); // supersingular iff P = 2 mod 3
+    assert!(inst.is_supersingular(&EllipticCurve::new_reduced_weierstrass(K::from_int(1), K::from_int(0)))); // supersingular iff P = 3 mod 4
+    assert!(inst.is_supersingular(&EllipticCurve::new_reduced_weierstrass(K::from_int(0), K::from_int(1)))); // supersingular iff P = 2 mod 3
 }
 
 #[test]
@@ -54,7 +64,7 @@ fn class_group_action_keep_valid() {
         for j in 0..6{
             action[j] = rand::random::<i32>()%3;
         }
-        assert!(verify_public_key(&inst, class_group_action(&inst, pk, action)));
+        assert!(inst.verify_public_key(inst.class_group_action(pk, action)));
     }
 
 }
@@ -76,12 +86,12 @@ fn class_group_action_commute(){
         }
 
 
-        let pk1 = class_group_action(&inst, pk.clone(), action1.clone());
-        let pk12 = class_group_action(&inst, pk1.clone(), action2.clone());
+        let pk1 = inst.class_group_action(pk.clone(), action1.clone());
+        let pk12 = inst.class_group_action(pk1.clone(), action2.clone());
 
 
-        let pk2 = class_group_action(&inst, pk, action2.clone());
-        let pk21 = class_group_action(&inst, pk2, action1.clone());
+        let pk2 = inst.class_group_action(pk, action2.clone());
+        let pk21 = inst.class_group_action(pk2, action1.clone());
 
         assert_eq!(pk12, pk21);
     }
@@ -91,8 +101,6 @@ fn class_group_action_commute(){
 fn naive_class_group_action_commute(){
     let inst = get_global_instance();
     let N_PRIMES = inst.n_primes;
-
-    check_well_defined(&inst);
 
     for _i in 0..10{
         let mut pk = K::from_int(0);
@@ -106,12 +114,12 @@ fn naive_class_group_action_commute(){
         }
 
 
-        let pk1 = naive_class_group_action(&inst, pk.clone(), action1.clone());
-        let pk12 = naive_class_group_action(&inst, pk1.clone(), action2.clone());
+        let pk1 = inst.naive_class_group_action(pk.clone(), action1.clone());
+        let pk12 = inst.naive_class_group_action(pk1.clone(), action2.clone());
 
 
-        let pk2 = naive_class_group_action(&inst, pk, action2.clone());
-        let pk21 = naive_class_group_action(&inst, pk2, action1.clone());
+        let pk2 = inst.naive_class_group_action(pk, action2.clone());
+        let pk21 = inst.naive_class_group_action(pk2, action1.clone());
 
         assert_eq!(pk12, pk21);
     }
@@ -138,7 +146,7 @@ fn isogeny_kernel(){
             continue;
         }
 
-        let (_, q) = isogeny(&ell, &p, p.clone(), inst.l[j].clone()).unwrap();
+        let (_, q) = CSIDHInstance::isogeny(&ell, &p, p.clone(), inst.l[j].clone()).unwrap();
         assert_eq!(UnsignedProjPoint::infinite_point(), q);
     }
 }
